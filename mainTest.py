155d0af0b341c9bb0121e5dcfe17db344ce12a12
lamlocan5 <dolam.dev.acc@gmail.com>
2024-08-15 11:56:35 +0700

    latest update

--- mainTest.py
+++ mainTest.py
@@ -1,11 +1,11 @@
 import cv2
-from keras import load_model
+from keras.models import load_model
 from PIL import Image
 import numpy as np
 
-model=load_model('BarinTumor10Epochs.h5')
+model=load_model('Braintumor10EpochsCategorical.h5')
 
-image=cv2.imread('D:\\DeepLearningProject\Brain Tumor Image Classification\\pred\pred0.jpg')
+image=cv2.imread(r'C:/Users/ADMIN/OneDrive/Máy tính/Project/2024 Project/BrainTumor/BRT350 - Copy/brain_tumor/test.jpg')
 
 img=Image.fromarray(image)
 
@@ -17,5 +17,5 @@ print(img)
 
 input_img=np.expand_dims(img, axis=0)
 
-result=model.predict_classes(img)
+result=model.predict_classes(input_img)
 print(result)
